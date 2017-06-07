647f5a50f14af33a00748e35ccfd1937268056fc
Roy Moore <roy@moore.co.il>
2017-06-07 19:07:18 +0300

    Back to basics

--- NAM/main.cpp
+++ NAM/main.cpp
@@ -1,40 +1,14 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <locale>
-#include <codecvt>
-#include <sstream>
-
-#include <io.h>    // for _setmode
-#include <fcntl.h> // for _O_U16TEXT
 
 using namespace std;
 
 int main() {
-	//system("chcp 65001 >nul");
-
-	int oldMode = _setmode(_fileno(stdout), _O_U16TEXT);
-	const char* filename = "Temp.map";
-	
-	wifstream map_file;
+	const char* filename = "temp.map";
+	fstream map_file;
 	map_file.open(filename);
-	locale utf8_locale(locale(), new codecvt_utf8<wchar_t>);
-	map_file.imbue(utf8_locale);
 
-	wstringstream contents;
-	int cols;
-	int rows;
-
-	map_file >> cols >> rows >> contents;
-	wcout << cols << endl;
-	wcout << rows << endl;
-	wcout << contents << endl;
-
-	//while (getline(map_file, line)) {
-	//	wcout << line << endl;
-	//}
-
-
+	cout << map_file.rdbuf();
 
 	map_file.close();
 
