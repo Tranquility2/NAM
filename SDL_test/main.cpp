856db83b3411763769d77661d5989bd6b927a909
Roy Moore <roy@moore.co.il>
2017-09-21 16:13:19 +0300

    Added SDL2 Image Lib

--- SDL_test/main.cpp
+++ SDL_test/main.cpp
@@ -1,28 +1,40 @@
 #include <SDL.h>
+#include <SDL_image.h>
 #include <iostream>
+#include <string>
 
 const int SCREEN_WIDTH = 720;
 const int SCREEN_HIGHT = 480;
 
 bool Initialized();
 void Close();
+SDL_Texture* LoadTexture(std::string file);
 
 SDL_Window* window = NULL;
-SDL_Surface* screen = NULL;
+//SDL_Surface* screen = NULL;
+SDL_Renderer* renderer = NULL;
+SDL_Texture* texture = NULL;
 
 int main(int argc, char* args[])
 {
 	if (!Initialized())
 	{
-		std::cout << "Could not initilized!" << std::endl;
+		std::cout << "Could not initialize!" << std::endl;
 		return 1;
 	}
 
-	std::cout << "initilized..." << std::endl;
+	std::cout << "Initialized..." << std::endl;
 	bool exit_flag = false;
 	SDL_Event event;
 
-	Uint32 bgColor = SDL_MapRGB(screen->format, 0, 0, 0);
+	//Uint32 bgColor = SDL_MapRGB(screen->format, 0, 0, 0);
+
+	std::string image_file_name = "Sdl-logo.png";
+	texture = LoadTexture(image_file_name.c_str());
+	if (texture == NULL)
+	{
+		return 1;
+	}
 
 	while (!exit_flag)
 	{
@@ -34,8 +46,11 @@ int main(int argc, char* args[])
 			}
 		}
 
-		SDL_FillRect(screen, NULL, bgColor);
-		SDL_UpdateWindowSurface(window);
+		//SDL_FillRect(screen, NULL, bgColor);
+		//SDL_UpdateWindowSurface(window);
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, texture, NULL, NULL);
+		SDL_RenderPresent(renderer);
 	}
 
 	Close();
@@ -60,7 +75,23 @@ bool Initialized()
 		return false;
 	}
 
-	screen = SDL_GetWindowSurface(window);
+	//screen = SDL_GetWindowSurface(window);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	if (renderer == NULL)
+	{
+		std::cout << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+		return false;
+	}
+
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	
+	if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG))
+	{
+		std::cout << "SDL_Image could not be  initilized! SDL_Image_Error: " << IMG_GetError() << std::endl;
+		return false;
+	}
+
 	return true;
 }
 
@@ -70,4 +101,30 @@ void Close()
 	window = NULL;
 
 	SDL_Quit();
+}
+
+SDL_Texture * LoadTexture(std::string file)
+{
+	SDL_Texture* newTexture = NULL;
+	SDL_Surface* loadedSurface = IMG_Load(file.c_str());
+
+	if (loadedSurface == NULL)
+	{
+		std::cout << "Unable to load the image " << file.c_str() << "! SDL_Image_Error: " << IMG_GetError() << std::endl;
+	}
+	else
+	{
+		std::cout << "Loaded the image " << file.c_str() << std::endl;
+
+		newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+		
+		if (newTexture == NULL)
+		{
+			std::cout << "Unable to create the texture from " << file.c_str() << "! SDL_Image_Error: " << IMG_GetError() << std::endl;
+		}
+
+		SDL_FreeSurface(loadedSurface);
+	}
+
+	return newTexture;
 }
